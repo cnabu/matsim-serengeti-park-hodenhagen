59cf3460c8917c97e1bb985b6f0c11ed034ffb9d
cnabu <cnabu@leeds.ac.uk>
2024-05-23 17:08:07 +0100

    created one event handler: HistogramHandler.java & modified SimpleAnalysis.java to run HistogramHandler.java on 23 May 2024

--- src/main/java/org/matsim/analysis/SimpleAnalysis.java
+++ src/main/java/org/matsim/analysis/SimpleAnalysis.java
@@ -6,9 +6,16 @@ public class SimpleAnalysis {
     public static void main(String[] args){
 
         var handler = new LinkEventHandler2();
+        var histohandler = new HistrogramHandler();
         var manager = EventsUtils.createEventsManager();
         manager.addHandler(handler);
+        manager.addHandler(histohandler);
 
         EventsUtils.readEvents(manager,"C:\\Users\\arsal\\Documents\\IntelliJProject\\matsim-serengeti-park-hodenhagen\\scenarios\\serengeti-park-v1.0\\output\\output-serengeti-park-v1.0-run1\\serengeti-park-v1.0-run1.output_events.xml.gz");
+
+        for (var stringIntegerEntry : histohandler.histogram.entrySet()){
+            System.out.println(stringIntegerEntry.getKey()+ ":" + stringIntegerEntry.getValue());
+
+        }
     }
 }
